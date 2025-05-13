b29069dfbfcefbd4644e760bec2b8ed6a848a4bb
Enrico Jeroense <tsjpajalol@gmail.com>
2025-05-13 01:35:56 -0700

    Menu menu functions
    Made the main menu function. Still need other files to make the buttons work

---
+++ main_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	pass
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Not_Enough_Time_Game_Jam/TempFile") #Replace with file of the main game
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Not_Enough_Time_Game_Jam/TempFile") #Replace with file of the options
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Not_Enough_Time_Game_Jam/TempFile") #Replace with file of the credits
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
