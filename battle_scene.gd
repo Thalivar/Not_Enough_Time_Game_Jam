118198c7cfee5e2c35b3ada3c7726d9b8156e4ef
Enrico Jeroense <tsjpajalol@gmail.com>
2025-05-17 22:25:43 -0700

    Fixed battle_scene bug

--- battle_scene.gd
+++ battle_scene.gd
@@ -7,27 +7,46 @@ var sorted_array = []
 var players : Array[Character]
 
 func _ready():
+	# Initialize player array
 	for player in player_group.get_children():
-		players.append(player.character)
+		if player is Sprite2D and player.get("character") != null:
+			players.append(player.character)
+			
+			# Ensure queue is initialized
+			if player.character.queue.size() == 0:
+				player.character.queue_reset()
 	
 	sort_and_display()
 
 func sort_combined_queue():
-	var player_array = []
-	for player in players:
-		for i in player.queue:
-			player_array.append({"character" : player, "time" : i})
+	sorted_array = []
 	
-	sorted_array = player_array
-	player_array.sort_custom(sort_by_time)
+	# Create array of players and their times
+	for player in players:
+		for time in player.queue:
+			sorted_array.append({"character": player, "time": time})
+	
+	# Sort the array by time
+	sorted_array.sort_custom(sort_by_time)
 	
 func sort_by_time(a, b):
 	return a["time"] < b["time"]
 
 func update_timeline():
-	var index : int = 0
+	# Safety check
+	if sorted_array.size() == 0:
+		return
+		
+	# Update timeline display
+	var index = 0
 	for slot in timeline.get_children():
-		slot.find_child("TextureRect").texture = sorted_array[index]["character"].icon
+		if index >= sorted_array.size():
+			break
+			
+		var texture_rect = slot.find_child("TextureRect")
+		if texture_rect and sorted_array[index]["character"].icon:
+			texture_rect.texture = sorted_array[index]["character"].icon
+		
 		index += 1
 
 func sort_and_display():
